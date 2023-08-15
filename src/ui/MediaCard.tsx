3b985b701412d9de41f7a8b475f104a6b1b0a1d5
Andrey Burov <AnBurov@Korusconsulting.ru>
2023-08-16 00:49:14 +0300

    stable with audio

--- src/ui/MediaCard.tsx
+++ src/ui/MediaCard.tsx
@@ -1,54 +1,69 @@
-import { RadioType } from '../types/radioTypes';
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import { RadioType } from '../types/radioTypes'
+import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Button } from 'reactstrap'
 
 type MediaCardProps = {
-  el: RadioType;
-};
+	el: RadioType
+}
 
 export default function MediaCard({ el }: MediaCardProps) {
-  const navigate = useNavigate();
-  const [showCard, setShowCard] = useState(false);
-  const [showButton, setShowButton] = useState(false);
+	const navigate = useNavigate()
+	const [showCard, setShowCard] = useState(false)
+	const [showButton, setShowButton] = useState(false)
 
-  const navigateHandler = (id) => {
-    navigate(`/${id}`);
-  };
+	const [isPlaying, setIsPlaying] = React.useState(false)
 
-  const handleMouseEnter = () => {
-    setShowButton(true);
-  };
+	const handleTogglePlay = () => {
+		setIsPlaying(!isPlaying)
+	}
 
-  const handleMouseLeave = () => {
-    setShowButton(false);
-  };
+	const navigateHandler = id => {
+		navigate(`/${id}`)
+	}
 
-  return (
-    <div className="media-card-container">
-      <div
-        className="media-card"
-        onMouseEnter={() => setShowCard(true)}
-        onMouseLeave={() => setShowCard(false)}
-      >
-        <div className="media-card-image-container">
-          <img
-            className="media-card-image"
-            alt="Cover"
-            src={el.favicon === '' ? '/img/cover.jpeg' : el.favicon}
-          />
-        </div>
-        <div className="media-card-content">
-          <h5 className="media-card-title">{el.name}</h5>
-          <p className="media-card-subtitle">{el.votes} Подписчиков</p>
-          <p className="media-card-subtitle">{el.country}</p>
-        </div>
-        <button
-          className="media-card-button"
-          onClick={() => navigateHandler(el.id)}
-        >
-          Слушать
-        </button>
-      </div>
-    </div>
-  );
+	const handleMouseEnter = () => {
+		setShowButton(true)
+	}
+
+	const handleMouseLeave = () => {
+		setShowButton(false)
+	}
+
+	return (
+		<div className="media-card-container">
+			<div
+				className="media-card"
+				onMouseEnter={() => setShowCard(true)}
+				onMouseLeave={() => setShowCard(false)}
+			>
+				<div className="media-card-image-container">
+					<img
+						className="media-card-image"
+						alt="Cover"
+						src={el.favicon === '' ? '/img/cover.jpeg' : el.favicon}
+					/>
+				</div>
+				<div className="media-card-content">
+					<h5 className="media-card-title">{el.name}</h5>
+					<p className="media-card-subtitle">
+						{el.votes} Подписчиков
+					</p>
+					<p className="media-card-subtitle">{el.country}</p>
+				</div>
+				{isPlaying ? (
+					<audio src={el?.url} controls autoPlay>
+						Ваш браузер не поддерживает элемент аудио.
+					</audio>
+				) : (
+					<button
+						className="media-card-button"
+						onClick={handleTogglePlay}
+					>
+						Слушать
+					</button>
+				)}
+			</div>
+		</div>
+	)
 }
