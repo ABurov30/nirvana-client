b673d90d18d800fb1553a082124337de1b6d447d
Andrey Burov <AnBurov@Korusconsulting.ru>
2023-10-22 19:48:25 +0300

    stable with UI kit

--- src/pages/RadiosPage/RadiosPage.tsx
+++ src/pages/RadiosPage/RadiosPage.tsx
@@ -1,22 +1,31 @@
-import LeftArrowButton from '../../ui/LeftArrowButton/LeftArrowButton'
-import { getAllRadiosThunk, searchRadioThunk } from './radiosThunk'
+import {
+	HomeNavbarButton,
+	LikeRoundButton,
+	MixBlockButton,
+	Play,
+	PlayBlockButton,
+	ThemeSlider,
+	UserPanel,
+	VolumeSlider
+} from 'radio-app-uikit'
 import { useGetAllRadios } from '../../hooks/useGetAllRadios'
-import ShareButton from '../../ui/ShareButton/ShareButton'
-import LikeButton from '../../ui/LikeButton/LikeButton'
-import { useAppSelector } from '../../redux/hooks'
 import React, { lazy, useEffect } from 'react'
-import { RoundButton } from 'radio-app-uikit'
-import { useDispatch } from 'react-redux'
 import Form from '../../ui/Form/Form'
 const MediaCard = lazy(() => import('../../ui/MediaCard'))
-
 
 export default function RadiosPage(): JSX.Element {
 	const radios = useGetAllRadios()
 
 	return (
 		<>
-			<RoundButton/>
+			<LikeRoundButton />
+			<PlayBlockButton />
+			<ThemeSlider />
+			<UserPanel name={'fdaf'} nickname="fdsa" amountOfMessages={3} />
+			<VolumeSlider />
+			<HomeNavbarButton/>
+			<MixBlockButton />
+			{/* <Play /> */}
 			<Form />
 			<div className="cards-container">
 				{radios?.map(el => (
