e9f73d8cb5cfee3e7dd0f00c1ac22571d51dfe28
Andrey Burov <AnBurov@Korusconsulting.ru>
2024-02-13 18:18:43 +0300

    little fix

--- src/shared/Request/Requets.ts
+++ src/shared/Request/Requets.ts
@@ -1,51 +1,31 @@
-import axios, {
-	type AxiosProgressEvent,
-	type AxiosRequestConfig,
-	type AxiosResponse
-} from 'axios'
-import MockAdapter from 'axios-mock-adapter'
+import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios'
 
 import { type IRequestParams } from './types'
 
 const NewInstanse = axios.create({
-	// @ts-ignore
+	//@ts-ignore
 	baseURL: import.meta.env.VITE_BASE_URL,
 	withCredentials: true
 })
 
 class Request {
-	private readonly controller = new AbortController()
-	private readonly mock = new MockAdapter(axios)
-
 	async sendRequest(
 		{ method = 'get', url, data, responseType = 'json' }: IRequestParams,
-		options: AxiosRequestConfig & { mockData?: AxiosResponse } = {}
+		options: AxiosRequestConfig
 	): Promise<AxiosResponse> {
 		const requestOptions = {
 			method,
 			url,
 			data,
 			responseType,
-			onDownloadProgress: this.getDowloadProgress,
 			...options
 		}
-		// @ts-ignore
+		//@ts-ignore
 		return await NewInstanse(requestOptions)
 			.then(function (response: AxiosResponse) {
 				return response
 			})
 			.catch(error => error.response)
-	}
-
-	getDowloadProgress(progressEvent: AxiosProgressEvent) {
-		let percentCompleted = 0
-		if (progressEvent.total !== undefined) {
-			percentCompleted = Math.round(
-				(progressEvent.loaded * 100) / progressEvent.total
-			)
-		}
-
-		return percentCompleted
 	}
 }
 
