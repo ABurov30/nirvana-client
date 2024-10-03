189c2e59dee600bee4186f35bf67b7b8b5451894
Andrey Burov <anburov@mons.ru>
2024-10-04 00:07:41 +0300

    deploy

--- src/widgets/Messenger/Messenger.tsx
+++ src/widgets/Messenger/Messenger.tsx
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import { useState } from 'react'
 
-import { BlockButton } from 'nirvana-uikit'
+import ChatIcon from '@mui/icons-material/Chat'
 
 import { useAppSelector } from 'shared/Redux/hooks'
 
@@ -13,33 +13,37 @@ export const Messenger = () => {
 
 	return (
 		<>
-			{isOpen && (
-				<div className={styles.messenger}>
-					<iframe
-						width="100%"
-						height="100%"
-						id="messenger"
-						src={import.meta.env.VITE_MESSENGER_IFRAME_URL}
-						onLoad={() => {
-							const messageWindow =
-								document.querySelector('#messenger')
-							messageWindow.contentWindow.postMessage(
-								{ message: 'init', user },
-								import.meta.env.VITE_MESSENGER_IFRAME_URL
-							)
-							console.log(
-								'🚀 ~ Messenger ~ messageWindow:',
-								messageWindow
-							)
-						}}
-					/>
-				</div>
+			{isOpen ? (
+				<>
+					<div
+						className={styles.shadow}
+						onClick={() => setIsOpen(false)}
+					></div>
+					<div className={styles.messenger}>
+						<iframe
+							width="100%"
+							height="100%"
+							id="messenger"
+							src={import.meta.env.VITE_MESSENGER_IFRAME_URL}
+							onLoad={() => {
+								const messageWindow =
+									document.querySelector('#messenger')
+								messageWindow.contentWindow.postMessage(
+									{ message: 'init', user },
+									import.meta.env.VITE_MESSENGER_IFRAME_URL
+								)
+							}}
+						/>
+					</div>
+				</>
+			) : (
+				<button
+					className={styles.button}
+					onClick={() => setIsOpen(true)}
+				>
+					<ChatIcon />
+				</button>
 			)}
-
-			<BlockButton
-				text="Open messenger"
-				onClick={() => setIsOpen(prev => !prev)}
-			/>
 		</>
 	)
 }
